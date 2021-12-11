406d0576730ec45915e3ee1028b759b1ef0b0f81
hritikb27 <hritikb90@gmail.com>
2021-12-12 01:28:52 +0530

    Added Chat UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import SignUp from "./components/SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import ChatUI from "./components/ChatUI";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/chat" element={<ChatUI />} />
         </Routes>
       </BrowserRouter>
 		</div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Login(){
-    return(
-        <div className="bg-grey-lighter min-h-screen flex flex-col">
-				<div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-					<div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-						<h1 className="mb-8 text-3xl text-center">Login</h1>
+function Login() {
+	return (
+		<div className="bg-grey-lighter min-h-screen flex flex-col">
+			<div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+				<div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+					<h1 className="mb-8 text-3xl text-center">Login</h1>
 
-						<input
-							type="text"
-							className="block border border-grey-light w-full p-3 rounded mb-4"
-							name="email"
-							placeholder="Email"
-						/>
+					<input
+						type="text"
+						className="block border border-grey-light w-full p-3 rounded mb-4"
+						name="email"
+						placeholder="Email"
+					/>
 
-						<input
-							type="password"
-							className="block border border-grey-light w-full p-3 rounded mb-4"
-							name="password"
-							placeholder="Password"
-						/>
-					
-						<button
-							type="submit"
-							className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1"
-						>
+					<input
+						type="password"
+						className="block border border-grey-light w-full p-3 rounded mb-4"
+						name="password"
+						placeholder="Password"
+					/>
+
+					<Link to="/chat">
+						<a className="w-full text-center py-3 rounded bg-green text-white hover:bg-green-dark focus:outline-none my-1">
 							Login
-						</button>
-
-						</div>
-					</div>
-
+						</a>
+					</Link>
+				</div>
 			</div>
-    )
+		</div>
+	);
 }
 
 export default Login;
